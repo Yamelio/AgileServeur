729d8e6e5e5969b848e76406ba0c4e96ae4dbc4f
Guillaume Kleinpoort <guillaume.kleinpoort@gmail.com>
2016-03-24 17:26:06 +0100

    Serveur update j2

--- src/main/java/fr/iutinfo/skeleton/api/VoeuxDao.java
+++ src/main/java/fr/iutinfo/skeleton/api/VoeuxDao.java
@@ -9,9 +9,11 @@ import java.util.List;
 public interface VoeuxDao {
 	
 	
-	@SqlQuery("select u.login,u.prenom,u.nom,t.lib as role,f.diplome,f.domaine,e.eno,f.fno,e.nom as etablissement,e.ville from utilisateur as u,formation as f,etablissement as e, voeux as v, formEtabl as fe, type as t where u.login=v.login and v.feno=fe.feno and fe.fno=f.fno and fe.eno=e.eno and t.tno=u.type;")
+	@SqlQuery("select u.login,u.prenom,u.nom,t.lib as role,f.diplome,f.domaine,e.eno,f.fno,e.nom as etablissement,e.ville "
+			+ "from utilisateur as u,formation as f,etablissement as e, voeux as v, formEtabl as fe, type as t "
+			+ "where u.login=v.login and v.feno=fe.feno and fe.fno=f.fno and fe.eno=e.eno and t.tno=u.type and u.login=:login;")
 	@RegisterMapper(VoeuMapper.class)
-	List<Voeu>getVoeuxByLogin(String login);
+	List<Voeu>getVoeuxByLogin(@Bind("login") String login);
 	
 	/*
 	@SqlQuery("")
--- src/main/java/fr/iutinfo/skeleton/api/VoeuxResource.java
+++ src/main/java/fr/iutinfo/skeleton/api/VoeuxResource.java
@@ -8,38 +8,43 @@ import java.util.List;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
-
 @Path("/voeux")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class VoeuxResource {
-	
+
 	private static VoeuxDao dao = BDDFactory.getDbi().open(VoeuxDao.class);
-    final static Logger logger = LoggerFactory.getLogger(SecureResource.class);
+	final static Logger logger = LoggerFactory.getLogger(SecureResource.class);
 
-    @GET
-    public List<Voeu> getVoeux(String login) {
-    	return dao.getVoeuxByLogin(login);    	 
-    }
+	@GET
+	@Path("{login}")
+	public Response getVoeux(@PathParam("login") String login) {
+		List<Voeu> reponse = dao.getVoeuxByLogin(login);
+		return Response.status(200).entity(reponse.toString()).build();
 
-    /*
-    @PUT
-    public boolean addVoeuTo(String login, Voeu voeu) {
-        return dao.addVoeuTo(login,voeu);
-        
-    }
+	}
 
-    @DELETE
-    public Voeu removeVoeuTo(String login,Voeu voeu) {
-        return dao.removeVoeuTo(login,voeu);
-    }
-	*/
+	/*
+	 * @PUT public boolean addVoeuTo(String login, Voeu voeu) { return
+	 * dao.addVoeuTo(login,voeu);
+	 * 
+	 * }
+	 * 
+	 * @DELETE public Voeu removeVoeuTo(String login,Voeu voeu) { return
+	 * dao.removeVoeuTo(login,voeu); }
+	 */
 }
