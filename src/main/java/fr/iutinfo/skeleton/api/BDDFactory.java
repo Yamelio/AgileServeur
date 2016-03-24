976a39958108b3191bcba0a2a3595c9e5e3bcc7f
Guillaume Kleinpoort <guillaume.kleinpoort@gmail.com>
2016-03-24 16:10:10 +0100

    MICHELINE MARCHE

--- src/main/java/fr/iutinfo/skeleton/api/BDDFactory.java
+++ src/main/java/fr/iutinfo/skeleton/api/BDDFactory.java
@@ -1,20 +1,37 @@
 package fr.iutinfo.skeleton.api;
 
-import org.skife.jdbi.v2.DBI;
-import org.sqlite.SQLiteDataSource;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.inject.Singleton;
 
+import org.postgresql.ds.PGPoolingDataSource;
+import org.skife.jdbi.v2.DBI;
+
 @Singleton
 public class BDDFactory {
-    private static DBI dbi = null;
 
-    public static DBI getDbi() {
-        if(dbi == null) {
-            SQLiteDataSource ds = new SQLiteDataSource();
-            ds.setUrl("jdbc:sqlite:" + System.getProperty("java.io.tmpdir") + System.getProperty("file.separator") + "data.db");
-            dbi = new DBI(ds);
-        }
-        return dbi;
-    }
+	private static DBI dbi = null;
+
+	public static DBI getDbi() {
+		if (dbi == null) {
+
+			PGPoolingDataSource ds = new PGPoolingDataSource();
+			ds.setServerName("psqlserv");
+			ds.setDatabaseName("post_iut");			
+			ds.setUser("ngoutans");
+			ds.setPassword("moi");
+			ds.setPortNumber(5432);
+			
+			
+			System.out.println(ds.getUser());
+			System.out.println(ds.getPortNumber());
+			System.out.println(ds.getDescription());
+
+			dbi = new DBI(ds);
+		}
+		return dbi;
+	}
 }
--- src/main/java/fr/iutinfo/skeleton/api/VoeuxDao.java
+++ src/main/java/fr/iutinfo/skeleton/api/VoeuxDao.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface VoeuxDao {
 	
 	
-	@SqlQuery("select u.login,u.prenom,u.nom,t.lib,f.diplome,f.domaine,e.eno,f.fno,e.nom,e.ville from utilisateur as u,formation as f,etablissement as e, voeux as v, formEtabl as fe, type as t where u.login=v.login and v.feno=fe.feno and fe.fno=f.fno and fe.eno=e.eno and t.tno=u.type;")
+	@SqlQuery("select u.login,u.prenom,u.nom,t.lib as role,f.diplome,f.domaine,e.eno,f.fno,e.nom as etablissement,e.ville from utilisateur as u,formation as f,etablissement as e, voeux as v, formEtabl as fe, type as t where u.login=v.login and v.feno=fe.feno and fe.fno=f.fno and fe.eno=e.eno and t.tno=u.type;")
 	@RegisterMapper(VoeuMapper.class)
 	List<Voeu>getVoeuxByLogin(String login);
 	
