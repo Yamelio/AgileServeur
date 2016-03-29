a67c7ed2cfb7393756f8964c5b904f1e8d67d1ee
Guillaume Kleinpoort <guillaume.kleinpoort@gmail.com>
2016-03-29 08:57:07 +0200

    update get etudiant

--- src/main/java/fr/iutinfo/skeleton/api/LoginDao.java
+++ src/main/java/fr/iutinfo/skeleton/api/LoginDao.java
@@ -1,5 +1,19 @@
 package fr.iutinfo.skeleton.api;
 
-public class LoginDao {
+import org.skife.jdbi.v2.sqlobject.*;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 
+
+import java.util.List;
+
+public interface LoginDao {
+	
+	
+	@SqlQuery(";")
+	@RegisterMapper(VoeuMapper.class)
+	List<Voeu>getVoeuxByLogin(@Bind("login") String login);
+	
+	
+	
+	void close();
 }
