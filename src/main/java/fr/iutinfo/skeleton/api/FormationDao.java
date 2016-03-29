4731ae58272364bbc299e3818213656b3c0c52b3
Guillaume Kleinpoort <guillaume.kleinpoort@gmail.com>
2016-03-29 15:48:58 +0200

    update

--- src/main/java/fr/iutinfo/skeleton/api/FormationDao.java
+++ src/main/java/fr/iutinfo/skeleton/api/FormationDao.java
@@ -33,7 +33,9 @@ public interface FormationDao {
 	
 	void close();
 
-	@SqlUpdate("insert into formEtabl (fno,eno) values (:fno,:eno);")
+	@SqlUpdate("insert into formEtabl(fno,eno) select :fno,:eno"
+			+ "where not exists"
+			+ "(select eno,fno from formEtabl where eno=:eno and fno=:fno);")
 	void addFormEtabl(@Bind("fno")int fno,@Bind("eno") int eno);
 
 	
